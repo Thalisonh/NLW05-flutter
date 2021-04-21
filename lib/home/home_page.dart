b8da752cde0678123c09a34dcba2740c33eb1e87
thalisonh <thalison_henrique@hotmail.com>
2021-04-20 23:38:25 -0300

    Segundo dia da NLW05

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:devquiz/widgets/appbard/app_bar_widget.dart';
+import 'package:devquiz/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key ? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -12,6 +14,48 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
